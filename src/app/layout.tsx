fc0914a2cd90f66e8013deb7d027a758b8e5e3d5
Adriano Maringolo <adrianomaringolo@gmail.com>
2025-09-11 13:52:23 -0300

    feat: replacing favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@/hooks/use-theme'
 import { Analytics } from '@vercel/analytics/next'
 import type { Metadata } from 'next'
 import { Lato } from 'next/font/google'
+import Image from 'next/image'
 import type React from 'react'
 import { Suspense } from 'react'
 import './globals.css'
@@ -16,10 +17,28 @@ const lato = Lato({
 })
 
 export const metadata: Metadata = {
-  title: 'Portfólio - Desenvolvedor Web',
+  title: 'Adriano Maringolo - Desenvolvedor Web',
   description:
-    'Portfólio pessoal de desenvolvedor web especializado em React, Next.js e TailwindCSS',
-  generator: 'v0.app',
+    'Desenvolvedor web especializado na criação de sites e aplicações de alta performance com React, Next.js e TailwindCSS.',
+  keywords:
+    'Adriano Maringolo, desenvolvedor web, portfolio, React, Next.js, TailwindCSS, JavaScript, TypeScript, desenvolvimento de sites, aplicações web',
+  authors: [{ name: 'Adriano Maringolo' }],
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://adrianomaringolo.dev',
+    title: 'Adriano Maringolo - Desenvolvedor Web',
+    description:
+      'Desenvolvedor web especializado na criação de sites e aplicações de alta performance com React, Next.js e TailwindCSS.',
+    images: [
+      {
+        url: 'https://adrianomaringolo.dev/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Adriano Maringolo - Desenvolvedor Web',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
@@ -32,7 +51,19 @@ export default function RootLayout({
       <body className={`font-sans ${lato.className} `}>
         <ThemeProvider>
           <LocaleProvider>
-            <Suspense fallback={<div>Loading...</div>}>
+            <Suspense
+              fallback={
+                <div className="w-dvw h-dvh flex justify-center items-center">
+                  <Image
+                    src="/logo.png"
+                    alt="Logo"
+                    width={50}
+                    height={50}
+                    className="animate-ping"
+                  />
+                </div>
+              }
+            >
               <main className="min-h-screen relative pb-20">
                 <Navbar />
                 <section className="pt-16">{children}</section>
